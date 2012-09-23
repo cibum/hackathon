b7e1d5bbfb78629d773ccd9790ddb882a118d4df
Carlos Zúñiga <czuniga@stormlabs-peru.com>
2012-09-22 20:28:02 -0500

    Agregada biblioteca de socrata

--- app/autoload.php
+++ app/autoload.php
@@ -18,6 +18,7 @@ $loader->registerNamespaces(array(
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
     'Twig_'            => __DIR__.'/../vendor/twig/lib',
+    'Socrata'          => __DIR__.'/../vendor/socrata/public/',
 ));
 
 // intl
