9086c346888edaaaa2ec920b68bc2ff6ff568734
Ernesto José Vargas Paz <ejosvp@ejosvp.(none)>
2012-09-22 20:42:35 -0500

    map map map

--- src/Cibum/ConcursoBundle/Resources/js/client.js
+++ src/Cibum/ConcursoBundle/Resources/js/client.js
@@ -1,9 +1,9 @@
 $(function(){
-    $('#limaMap').gmap3(
+    $('#map').gmap3(
 	{ action:'init',
 	  options:{
 	      center:[-12.047816,-77.062203],
-	      zoom:5
+	      zoom:11
 	  }
-    );
+    });
 });
